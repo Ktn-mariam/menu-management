c888c0d1175a4be1340ed216a0988fd8c3398cef
Mariam Khatoon <mariamkhatoon996@gmail.com>
2025-01-30 20:16:35 +0400

    feat: updateCategory controller and route added

--- controllers/category.ts
+++ controllers/category.ts
@@ -1,29 +1,68 @@
-import {Request, Response} from 'express'
+import { Request, Response } from 'express';
 import Category from '../models/category';
 import { StatusCodes } from 'http-status-codes';
 
 
 // To create a category
 const addCategory = async (req: Request, res: Response) => {
-  const category = await Category.create({...req.body})
+  const category = await Category.create({ ...req.body });
 
-  res.status(StatusCodes.CREATED).json({category})
-}
+  res.status(StatusCodes.CREATED).json({ category });
+};
 
 
 // To get All Categories
 const getAllCategories = async (req: Request, res: Response) => {
-  const categories = await Category.find({}).select('_id name')
+  const categories = await Category.find({}).select('_id name');
 
-  res.status(StatusCodes.OK).json({categories, nbHits: categories.length})
-}
+  res.status(StatusCodes.OK).json({ categories, nbHits: categories.length });
+};
 
 // To get Category
 const getCategory = async (req: Request, res: Response) => {
-  const {categoryId} = req.params
-  const category = await Category.findOne({_id: categoryId })
-  
-  res.status(StatusCodes.OK).json({category})
-}
+  const { categoryId } = req.params;
+  const category = await Category.findOne({ _id: categoryId });
 
-export { addCategory, getAllCategories, getCategory }
+  res.status(StatusCodes.OK).json({ category });
+};
+
+// To update category
+const updateCategory = async (req: Request, res: Response) => {
+  const { categoryId } = req.params;
+
+  let updateFields: any = {};
+
+  if (req.body.name) updateFields.body = req.body.name;
+  if (req.body.image) updateFields.image = req.body.image;
+  if (req.body.description) updateFields.description = req.body.description;
+  if (Object.prototype.hasOwnProperty.call(req.body, 'taxApplicability')) updateFields.taxApplicability = req.body.taxApplicability;
+  if (req.body.tax) updateFields.tax = req.body.tax;
+  if (req.body.taxType) updateFields.taxType = req.body.taxType;
+
+  let updatedCategory;
+  if (Object.prototype.hasOwnProperty.call(req.body, 'taxApplicability') && !req.body.taxApplicability) {
+    updatedCategory = await Category.findByIdAndUpdate(categoryId, { $set: updateFields, $unset: { tax: 1, taxType: 1 } }, { new: true, runValidators: true });
+    
+
+    if (!updatedCategory){
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: 'Product not found' });
+      return
+    }
+  }
+
+  updatedCategory = await Category.findByIdAndUpdate(categoryId, { $set: updateFields }, { new: true, runValidators: true });
+
+  if (!updatedCategory){
+        res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ error: 'Product not found' });
+      return
+  }
+
+  res.json({ category: updatedCategory });
+};
+
+
+export { addCategory, getAllCategories, getCategory, updateCategory };
--- routes/category.ts
+++ routes/category.ts
@@ -3,11 +3,11 @@
 // API to get a category by name or ID along with its attributes
 // API to edit category attributes
 
-import {Router} from 'express'
-import { addCategory, getAllCategories, getCategory } from '../controllers/category'
-const router = Router()
+import { Router } from 'express';
+import { addCategory, getAllCategories, getCategory, updateCategory } from '../controllers/category';
+const router = Router();
 
-router.route('/').post(addCategory).get(getAllCategories)
-router.route('/:categoryId').get(getCategory)
+router.route('/').post(addCategory).get(getAllCategories);
+router.route('/:categoryId').get(getCategory).patch(updateCategory);
 
-export default router
+export default router;
