869dfb11cef9ab42a07fe2f449f0c7a05893a63d
Mariam Khatoon <mariamkhatoon996@gmail.com>
2025-01-30 12:35:31 +0400

    feat: model Schema created for Category

---
+++ models/category.ts
@@ -0,0 +1,39 @@
+import { Document, Schema, Types, model } from 'mongoose'
+
+// Name: String
+// Image: URL
+// Description: String
+// Tax Applicability: Boolean
+// Tax: Number, if applicable
+// Tax type
+
+const CategorySchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required to create a Category']
+  },
+  image: {
+    type: String,
+    required: [true, 'Name is required to create a Category']
+  },
+  description: {
+    type: String,
+    required: [true, 'Name is required to create a Category']
+  },
+  taxApplicability: {
+    type: Boolean,
+    required: [true, 'Name is required to create a Category']
+  },
+  tax: {
+    type: Number,
+    required: [true, 'Name is required to create a Category']
+  },
+  taxType: {
+    type: String,
+    enum: ['INCOME', 'WEALTH', 'GIFT', 'CAPITAL GAINS', 'SECURITIES TRANSACTION', 'CORPORATE'],
+    default: 'CORPORATE'
+  }
+})
+
+
+export default model('Category', CategorySchema)
