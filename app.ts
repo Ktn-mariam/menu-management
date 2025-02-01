fb79afd5f789355c5ef2378cb29565b731c8f695
Mariam Khatoon <mariamkhatoon996@gmail.com>
2025-02-01 18:39:08 +0400

    feat: Item router connected to the app

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import connectDB from './db/connect'
 import categoryRouter from './routes/category'
 import subCategoryRouter from './routes/subcategory'
+import itemRouter from './routes/item'
 
 const app = express()
 
@@ -11,6 +12,8 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/v1/category', categoryRouter)
 app.use('/api/v1/subCategory', subCategoryRouter)
+app.use('/api/v1/item', itemRouter)
+
 app.get('/', (req, res) => {
   res.send('Menu management app is running...')
 })
