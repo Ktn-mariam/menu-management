9f0afc1f079b1a636bef8fced85e1e10c3484cf6
Mariam Khatoon <mariamkhatoon996@gmail.com>
2025-02-02 18:18:41 +0400

    feat: installed security packages, and added them to the app

--- app.ts
+++ app.ts
@@ -1,4 +1,7 @@
 import express from 'express'
+import cors from 'cors'
+import helmet from "helmet";
+import rateLimit from "express-rate-limit"
 import 'dotenv/config'
 import connectDB from './db/connect'
 import categoryRouter from './routes/category'
@@ -9,6 +12,9 @@ import errorHandlerMiddleware from './middleware/error-handler';
 
 const app = express()
 
+app.use(helmet());
+app.use(cors());
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
